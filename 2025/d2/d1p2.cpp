028603a27200b423c4ba44816f4510227b5c2258
aaronboch <aaronboch8@gmail.com>
2025-12-06 15:50:16 +0100

    d2p2

--- 2025/d2/d1p2.cpp
+++ 2025/d2/d1p2.cpp
@@ -4,10 +4,11 @@
 #include <iostream>
 #include <sstream>
 std::vector<std::string> readFile(const std::string& filePath);
+bool checkInvalid(std::string num, int length);
 
 int main() {
-  std::vector<std::string> ranges = readFile("./input/test");
-  // std::vector<std::string> ranges = readFile("./input/input");
+  // std::vector<std::string> ranges = readFile("./input/test");
+  std::vector<std::string> ranges = readFile("./input/input");
   long result = 0;
   for (auto& range : ranges) {
     int pos = range.find("-");
@@ -17,21 +18,42 @@ int main() {
     std::string num;
     for (; lhs <= rhs; lhs++) {
       num = std::to_string(lhs);
-      if (num.length() % 2 != 0) {
-        continue;
-      }
-      //   std::cout << num << std::endl;
-      long left = stol(num.substr(0, num.length() / 2));
-      long right = stol(num.substr(num.length() / 2));
-      if (left == right) {
+      // std::cout << "num = " << num << std::endl;
+      if (checkInvalid(num, num.length())) {
+        std::cout << lhs << std::endl;
         result += lhs;
       }
-      //   std::cout << left << " " << right << std::endl;
-      //   std::cout << result << std::endl;
     }
   }
   std::cout << result << std::endl;
   return 0;
+}
+
+bool checkInvalid(std::string num, int length) {
+  for (int i = 1; i <= length / 2; i++) {
+    std::string toCheck = num.substr(0, i);
+    // std::cout << "to check:" << toCheck << std::endl;
+    // std::cout << "with i = " << i << std::endl;
+    for (int j = i; j <= length; j = j + i) {
+      std::string against = num.substr(j, i);
+      // std::cout << "against:" << against << std::endl;
+      // std::cout << "j:" << j << std::endl;
+      // std::cout << "length:" << length << std::endl;
+      if (j >= (float)length) {
+        // std::cout << "returned as invalid" << std::endl;
+        return true;
+      } else if (against == toCheck) {
+        // std::cout << "continued" << std::endl;
+        continue;
+      } else {
+        // std::cout << "break not invalid with i = " << i << std::endl;
+        break;
+      }
+    }
+    // std::cout << std::endl;
+  }
+  // std::cout << "returned as not invalid" << std::endl;
+  return false;
 }
 
 std::vector<std::string> readFile(const std::string& filePath) {
